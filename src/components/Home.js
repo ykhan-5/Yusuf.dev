c500539ce5169f8ad6ed48c9716ac177a16b25bb
Yusuf Khan <ykhan5@cougarnet.uh.edu>
2024-07-25 13:05:55 -0500

    Add Button and Button css

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,11 @@ const Home = () => {
         </div>
         <div className="card2">card2</div>
         <div className="card3">card3</div>
-        <div className="card4">card4</div>
+        <div className="card4">
+          <button class="button-73" role="button">
+            <span class="text">See my Resume!</span>
+          </button>
+        </div>
       </div>
     </div>
   );
