eabb8cbd8f5d5fdfa03cb420b4ed68e08619ee76
Ykhan-5 <yusufkhanhtx@gmail.com>
2026-02-01 11:38:06 -0600

    name change (domain)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',   // ← VERY IMPORTANT (repo name)
+  base: '/Yusuf.dev/',   // ← VERY IMPORTANT (repo name)
 })
